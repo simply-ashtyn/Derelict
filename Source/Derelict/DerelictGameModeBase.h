796b79d5c452c704d354111d892d6a0064f8db97
Ashtyn Lane <97042764+simply-ashtyn@users.noreply.github.com>
2025-10-01 14:13:54 -0500

    Project Setup

---
+++ Source/Derelict/DerelictGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DerelictGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DERELICT_API ADerelictGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
